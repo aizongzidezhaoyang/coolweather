5335ae3649a965fd76e532294ea5fa8fa8dd40bd
lzy <771545778@qq.com>
2018-10-30 10:38:26 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/coolweather/db/city.java
@@ -0,0 +1,38 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class city extends LitePalSupport {
+    private int id;
+    private String citName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCitName() {
+        return citName;
+    }
+    public void setCitName(String citName) {
+        this.citName = citName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
